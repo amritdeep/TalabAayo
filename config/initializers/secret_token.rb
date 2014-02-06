5b42727607f2d79bf788db33a04a7d761ce0f980
Amrit Deep Dhungana <amritdeepdhungana@Amrits-MacBook-Pro.local>
2014-02-06 22:22:33 +0545

    Initialize project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Talabaayo::Application.config.secret_key_base = '2914583f7768acbcf1fef9b96f615940f402099d5c4253376b6db42b36578678eeb38d78a1b71e20cfa552c34a0754447f2b9c063f31bda3c7d112b7ac85405d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Talabaayo::Application.config.session_store :cookie_store, key: '_talabaayo_session'
